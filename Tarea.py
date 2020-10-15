9f8f32efaa72d737b439f391ef4fca53c5525d40
PabloMunozP <pablo.munoz@2mail.udp.cl>
2020-10-15 17:11:20 -0300

    .

--- Tarea.py
+++ Tarea.py
@@ -1,4 +1,5 @@
 from Crypto.Cipher import DES
+import codecs 
 
 key = 'Secretos'#El tamaño maximo debe ser de 8bytes
 plaintext = 'Texto plano'
@@ -8,15 +9,14 @@ bkey=bytes(key,'utf8')
 biv= bytes(iv,'utf8')
 bplain=bytes(plaintext,'utf8')
 
-cipher = DES.new(bkey, DES.MODE_CFB,biv)
-
-msg =  cipher.encrypt(bplain)
-
-cipher2=DES.new(bkey, DES.MODE_CFB,biv)
+cipher = DES.new(bkey, DES.MODE_CFB)
+iv=cipher.iv
+msg = cipher.encrypt(bplain)
+cipher2=DES.new(bkey, DES.MODE_CFB,iv)
 msg2=cipher2.decrypt(msg)
 
 
-print(msg,bkey.hex(),biv.hex())
+print(msg.hex(),bkey.hex(),iv.hex())
 print(msg2)
 html=open("index.html","w")
 html.write('''
@@ -32,7 +32,7 @@ html.write('''
             <div class="iv" id= "%s"> Texto iv </div>
         </body>
     </html>
-        '''% (bkey.hex(),msg.hex(),biv.hex()))
+        '''% (bkey.hex(),msg.hex(),iv.hex()))
 
 html.close()
 
--- script.js
+++ script.js
@@ -4,26 +4,44 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Pablo Munoz P.
-// @require      https://code.jquery.com/jquery-3.5.1.min.js
-// @require      https://cdn.jsdelivr.net/npm/node-forge@0.7.0/dist/forge.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/forge/0.10.0/forge.all.min.js
 // @match        https://pablomunozp.github.io/Tarea3-Cripto/
 // @grant        none
 // ==/UserScript==
 
 window.addEventListener('load', function(){
-
-    var id=document.getElementsByClassName('DES')[0].id;
-    console.log(id);
-
+    'use strict';
+    var ivmsg = document.getElementsByClassName('DES')[0].id;
     var key = document.getElementsByClassName('key')[0].id;
     var iv = document.getElementsByClassName('iv')[0].id;
-    console.log(key);
-    console.log(iv);
-    var decipher = forge.cipher.createDecipher('DES-CBC', key);
+    //console.log('msg: ' + ivmsg, 'key: '+key, 'iv: ' +iv);
+//##########################################################
+    var msg=ivmsg.slice(iv.length)
+    //console.log(CryptoJS.enc.Utf8.parse(key))
+
+
+    //var test = CryptoJS.DES.encrypt(msg,key,{iv: iv, mode: CryptoJS.mode.CFB})
+    //console.log(test.ciphertext.toString(CryptoJS.enc.Hex))
+    //var d=CryptoJS.DES.decrypt(msg,'Secretos',{iv: iv, mode: CryptoJS.mode.CFB});
+    //console.log(d, d.toString(CryptoJS.enc.Latin1));
+    //var words = CryptoJS.enc.Hex.parse("48656c6c6f2c20576f726c6421");
+    //var hex = CryptoJS.enc.Hex.stringify(words);
+    //console.log( words,hex);
+    var cipher = forge.cipher.createCipher('DES-CFB', key);
+    cipher.start({iv: iv});
+    cipher.update(forge.util.createBuffer('Texto plano','utf8'));
+    cipher.finish();
+    var encrypted = cipher.output;
+    // outputs encrypted hex
+    console.log(encrypted.toHex());
+
+
+    var decipher = forge.cipher.createDecipher('DES-CFB', key);
     decipher.start({iv: iv});
-    decipher.update(id);
+    decipher.update(encrypted);
     var result = decipher.finish(); // check 'result' for true/false
     // outputs decrypted hex
-    console.log(decipher.output.toHex());
-
+    console.log(decipher);
 });
